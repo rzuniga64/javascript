9382e62979b881354dc5b6e20a62a3c2ed216774
Raul Zuniga <rzuniga64@gmail.com>
2014-06-13 23:40:48 -0500

    Add udemy ch18-10 JavaScript examples

---
+++ udemy/scripts/ch17_persistence_storage/localstorage.js
@@ -0,0 +1,42 @@
+// session storage
+// data is never sent to the server so it is not a direct replacement for cookies
+var ss = window.sessionStorage;	    // temporary session storage
+                                    // particular to a browser tab or window
+var ls = window.localStorage;		// permanent storage
+
+/*
+Both support the following properties and methods:
+	setItem(name, value)	- add or update a value in the store
+	getItem(name)			- get a named value from the store
+	removeItem(name)		- remove a named value from the store
+	length					- number of values stored
+	key(index)				- name of the key at the index
+	clear()					- clear the store
+*/
+
+ss.setItem("a", 100);
+ss.setItem("b", 200);
+ss.setItem("c", 300);
+
+ls.setItem("x", 1000);
+ls.setItem("y", 2000);
+ls.setItem("z", 3000);
+
+ss.removeItem("b");
+
+ls.clear();
+
+// show sessionStorage values
+var k, msg = "<h2>sessionStorage (" + ss.length + " items)</h2>";
+for (k in ss) {
+	msg += "<p>" + k + " = " + ss.getItem(k) + "<p>";
+}
+
+// show localStorage values
+msg += "<h2>localStorage (" + ls.length + " items)</h2>";
+for (k in ls) {
+	msg += "<p>" + k + " = " + ls.getItem(k) + "<p>";
+}
+
+// output all values
+document.getElementById("output").innerHTML = msg;
