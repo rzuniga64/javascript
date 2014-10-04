0bb96d219860af67dc4bbbdc1fad8ba6b1f48ca8
Raul Zuniga <rzuniga64@gmail.com>
2014-10-04 04:22:14 -0500

    Update files

--- udemy_javascript/scripts/ch17_persistence_storage/cookies.js
+++ udemy_javascript/scripts/ch17_persistence_storage/cookies.js
@@ -1,15 +1,15 @@
 // cookies
 /*
 // native cookie handling in JavaScript
-document.cookie = "a=100; expires=Wed, 01 Jan 2020 00:00:00 GMT; path=/js/";
-document.cookie = "b=200; expires=Wed, 01 Jan 2020 00:00:00 GMT; path=/js/";
-document.cookie = "c=300; expires=Wed, 01 Jan 2020 00:00:00 GMT; path=/js/";
+document.cookie = "a=100; expires=Wed, 01 Jan 2020 00:00:00 GMT; path=/scripts/";
+document.cookie = "b=200; expires=Wed, 01 Jan 2020 00:00:00 GMT; path=/scripts/";
+document.cookie = "c=300; expires=Wed, 01 Jan 2020 00:00:00 GMT; path=/scripts/";
 
 // show cookies
 document.getElementById("output").textContent = document.cookie;
 */
 
-// using cookielib.js
+// using cookielib.scripts
 Cookie.Set("a", 101, 10);
 Cookie.Set("b", 202, 20);
 Cookie.Set("c", 303, 30);
