ef458baff81571539f088d108c0efddf64c36cce
Raul Zuniga <rzuniga64@gmail.com>
2014-10-18 10:33:13 -0500

    Update ITSE 1394 program-05 files

--- itse1394_jquery/program-05/js/program-05.js
+++ itse1394_jquery/program-05/js/program-05.js
@@ -1,3 +1,5 @@
+var clock = setInterval(function() {setClock()},1000);
+
 function setClassToHidden(options) {
     for (var i = 0; i < options.length; ++i) {
         if (options[i].checked) {
@@ -53,7 +55,7 @@ function displayTest()
         case "option6":
             setClassToHidden(options);
             document.getElementById("test6").setAttribute("class","show" );
-            browserInformation()
+            browserInformation();
             break;
         case "option7":
             setClassToHidden(options);
@@ -62,32 +64,31 @@ function displayTest()
         case "option8":
             setClassToHidden(options);
             document.getElementById("test8").setAttribute("class","show" );
-            document.getElementsByTagName("div")[15].addEventListener("click", handleDivClick);
             break;
         case "option9":
             setClassToHidden(options);
             document.getElementById("test9").setAttribute("class","show" );
-            domContent();
+            document.getElementById("para9").setAttribute("class", "show");
+            setClock()
             break;
         case "option10":
             setClassToHidden(options);
-            document.getElementById("test10").setAttribute("class","show" );
-            changeContent();
+            document.getElementById("test9").setAttribute("class","show" );
+            document.getElementById("para9").setAttribute("class", "hidden");
+            document.getElementById("test10").setAttribute("class","show");
             break;
         case "option11":
             setClassToHidden(options);
             document.getElementById("test11").setAttribute("class","show" );
-            createNode();
             break;
         case "option12":
             setClassToHidden(options);
             document.getElementById("test12").setAttribute("class","show" );
-            removeNode();
+
             break;
         case "option13":
             setClassToHidden(options);
             document.getElementById("test13").setAttribute("class","show" );
-            changeNodeList();
             break;
         default:
             setClassToHidden(options);
@@ -143,7 +144,6 @@ function changeLocation() {
  */
 function goBack() {
     document.getElementById("message4").innerHTML =  "You have visited " + history.length + " Web pages";
-    var back = history.back();
     history.back();
 }
 
@@ -162,10 +162,21 @@ function goForward() {
 
  */
 function browserInformation() {
-    document.getElementById("message6").innerHTML += "appName: " + navigator.appName + "<br />";
-    document.getElementById("message6").innerHTML += "appCodeName: " + navigator.appCodeName+ "<br />";
-    document.getElementById("message6").innerHTML += "product(engine name): " + navigator.product;
 
+    try{
+        var para =  document.getElementById("message6");
+        if (para.innerHTML == "") {
+            document.getElementById("message6").innerHTML += "appName: " + navigator.appName + "<br />";
+            document.getElementById("message6").innerHTML += "appCodeName: " + navigator.appCodeName + "<br />";
+            document.getElementById("message6").innerHTML += "product(engine name): " + navigator.product;
+        } else {
+            var message = document.getElementById("message6");
+            while (message.firstChild)
+                message.removeChild(message.firstChild);
+        }
+    } catch(e) {
+        window.alert(e);
+    }
 }
 
 /* 7. Use a confirm box to ask the user if they would like to see the following information from the navigator object:
@@ -181,11 +192,64 @@ function displayNavigatorObjectInfo() {
             document.getElementById("message7").innerHTML += "appCodeName: " + navigator.userAgent + "<br />";
             document.getElementById("message7").innerHTML += "product(engine name): " + navigator.platform;
         } else if (!option[0].checked) {
-            var checkbox =  document.getElementById("message7")
-            while (checkbox.firstChild)
-                checkbox.removeChild(checkbox.firstChild);
+            var message =  document.getElementById("message7");
+            while (message.firstChild)
+                message.removeChild(message.firstChild);
         }
     } catch(e) {
         window.alert(e);
     }
 }
+
+/*  8. Include a prompt box to ask the user to enter their favorite browser. Include the name of your personal favorite
+ *      browser as the default value. Display the results.
+ */
+function displayFavoriteBrowser(){
+    var fieldValue = document.forms[0][0].value;
+    if (fieldValue == "") {
+        document.getElementById("message8").innerHTML = "Your favorite browser is Mozilla Firefox.";
+        return false;
+    } else {
+        document.getElementById("message8").innerHTML = "Your favorite browser is " + fieldValue +".";
+    }
+    return true;
+}
+
+/*  9. 	Include a clock on your page that displays the current local time and advances each second.*/
+function setClock(){
+    document.getElementById("clock").value = getCurrentTime();
+    return document.getElementById("clock").innerHTML
+}
+
+function getCurrentTime() {
+    var time= new Date().toLocaleTimeString();
+    return time;
+}
+/*  10. Include a button on your page to stop/start the clock on your page. The text on the button
+ *      should display “Stop” when the clock is running and should display “Start” when the timer is
+ *      stopped. The start/stop functionality should be perpetual – meaning that the button can be
+ *      clicked multiple times.
+ */
+function startStopClock() {
+    if (document.getElementById("stopStart").innerHTML == "Stop") {
+        clearInterval(clock);
+        document.getElementById("stopStart").innerHTML = "Start";
+    } else if (document.getElementById("stopStart").innerHTML == "Start") {
+        clock = setInterval(function() {setClock()},1000);
+        document.getElementById("stopStart").innerHTML = "Stop";
+    }
+
+}
+
+/*  11.	Display three images on the page five seconds after the page is opened. */
+
+/*  12. Include three text boxes to enter values for the following cookies: name, school, and
+ *      hobby. Include a button that creates the three cookies (with values). Include three
+ *      more buttons. Each button deletes a cookie.
+ */
+
+/*  13.	Include a function that will check the existence of each cookie when the page is accessed.
+ *      If one or more of the cookies is not set, an alert box should notify the user to set the
+ *      unset cookie(s). If all three cookies are set, the alert box should display the contents
+ *      of each cookie.
+ */
