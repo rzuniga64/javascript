41b99148293f28782310c73285cfd8ec90430ee4
Raul Zuniga <rzuniga64@gmail.com>
2014-10-24 05:13:10 -0500

    Update ITSE 1394  files

--- itse2402/module3/competency3.2/scripts/TopStories.php
+++
@@ -1,8 +0,0 @@
-<?php
-$NewsSource=$_GET["agency"];
-header("Content-Type: text/xml");
-header("Content-Length: " . strlen(file_get_contents($NewsSource)));
-header("Cache-Control: no-cache");
-readfile($NewsSource);
-?>
-
