5979a7d41bac37c5129c6566aad466b3b82cce50
Raul Zuniga <rzuniga64@gmail.com>
2015-08-04 17:37:05 -0500

    Add itse 2402 files

---
+++ itse2402/module4/competency4.1/js/withdrawal.php
@@ -0,0 +1,47 @@
+<?php
+    include 'getBalance.php';
+    $returnValue = readBalance();
+?>
+<!DOCTYPE html >
+<head>
+    <title>Express Bank | Withdrawal</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta name="description" content="Competency 4.1-4.2 for ITSE 2402 | Balance">
+    <meta name="author" content="Raul Zuniga">
+    <!-- Mobile viewport optimized -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link href="../css/competency4_1.css" rel="stylesheet" type="text/css">
+    <!-- Include Modernizr in the head, before any other Javascript -->
+    <script src="../js/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div id="flag"></div>
+    </div>
+    <div class="container">
+        <div class="starter-template">
+            <form  method="post" action="subtractBalance.php" enctype="application/x-www-form-urlencoded" role="form" class="form">
+                <h1 id="h1">Express Bank </h1><br />
+                <fieldset class="scheduler-border">           
+                    <?php
+                        echo "<p>Your starting account balance: " ;
+                        printf ( "%01.2f", $returnValue ) ;
+                        echo "</p>" ;
+                    ?>
+                    <p>withdrawal amount:
+                        <input type="text" name= "withdrawalAmount" id="withdrawalAmount" />
+                    </p>
+                    <p><input type="submit" id="make" value="Make WithDrawal"/></p>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+    <div id="footer">
+        &copy; 2014 Raul Zuniga | <a href="mailto:raul.zuniga@g.austincc.edu">raul.zuniga@g.austincc.edu</a>
+        <script type="text/javascript" src="../../js/validation.js"></script>
+    </div>
+</body>
+</html>
+
+
+
