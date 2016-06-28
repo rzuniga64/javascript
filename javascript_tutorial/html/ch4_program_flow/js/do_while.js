0da86688989ff0308b6904071ea31f10b2f99b29
Raul Zuniga <rzuniga64@gmail.com>
2016-06-28 00:33:38 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch4_program_flow/js/do_while.js
+++ javascript_tutorial/html/ch4_program_flow/js/do_while.js
@@ -2,12 +2,12 @@
 var msg = document.getElementById("message");
 var num = String(prompt("Bank Account Number", ""));
 
-do {
+do { // loop executes at least once
     num = "0" + num;
 } while (num.length < 8);
 
-// This runs much faster
-num = ("00000000" + num).substr(-8);
+// This runs much faster than doing a loop
+// num = ("00000000" + num).substr(-8);
 
 // output factorial message
 msg.textContent = num;
