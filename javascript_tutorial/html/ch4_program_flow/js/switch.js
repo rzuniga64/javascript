54b946e9bfbbaba452b184cd293948af9d0c3e14
Raul Zuniga <rzuniga64@gmail.com>
2016-06-27 20:47:20 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch4_program_flow/js/switch.js
+++ javascript_tutorial/html/ch4_program_flow/js/switch.js
@@ -4,18 +4,18 @@ var name = prompt("What is your name?", "");
 var output = "";
 
 switch (name) {
-case "Jean":
-    output = "Bonjour";
-    break;
-case "Juan":
-    output = "Hola";
-    break;
-case "Giovanni":
-    output = "Ciao";
-    break;
-default:
-    output = "Hello";
-    break;
+    case "Jean":
+        output = "Bonjour";
+        break;
+    case "Juan":
+        output = "Hola";
+        break;
+    case "Giovanni":
+        output = "Ciao";
+        break;
+    default:
+        output = "Hello";
+        break;
 }
 
 output += " " + name + "!";
