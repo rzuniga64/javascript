f2fd601e2c94857c152d7c4f9883c81b849d8b2b
Raul Zuniga <rzuniga64@gmail.com>
2016-06-27 23:40:58 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch5_functions/js/functions.js
+++ javascript_tutorial/html/ch5_functions/js/functions.js
@@ -10,20 +10,19 @@ do {
     }
 } while (num);
 
-
 // calculate factorials
 // var factorial = function(num) {
 function factorial(num) {
-    'use strict';
+    // ensure that parameter is a Number
     num = Number(num);
     var f = 1;
 
-    if (num < 0) {
+    // ensure that the number is not negative
+    if (num < 0)
         return null;
-    }
 
-    for (var i = 2; i <= num; i++) {
+    for (var i = 2; i <= num; i++)
 		f *= i;
-	}
+
 	return f;
 }
