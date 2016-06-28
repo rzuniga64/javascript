fac48c9bc42521f51677043adaa1914c3f606801
Raul Zuniga <rzuniga64@gmail.com>
2016-06-27 20:58:30 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch4_program_flow/js/forloops.js
+++ javascript_tutorial/html/ch4_program_flow/js/forloops.js
@@ -3,9 +3,9 @@ var msg = document.getElementById("message");
 var num = Number(prompt("Factorial", 5));
 
 // counter, test, update
-for (var i = 2, output = 1; i <= num; i++) {
-	output *= i;
+for (var i = 2, factorial = 1; i <= num; i++) {
+	factorial *= i;
 }
 
 // output factorial message
-msg.textContent = num + "! = " + output;
+msg.textContent = num + "! = " + factorial;
