3f3f38b467059c946720047a2fd7b908a767c568
Raul Zuniga <rzuniga64@gmail.com>
2016-06-28 19:33:46 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch6_arrays/js/sorting.js
+++ javascript_tutorial/html/ch6_arrays/js/sorting.js
@@ -10,6 +10,7 @@ msg.innerHTML += "<p><strong>After sorting:</strong> [" + v + "]<p>";
 
 
 // anonymous ch5_functions
+// functions are just another value type so we don't really need named functions here.
 v.sort(function(a,b) {return a-b;});
 msg.innerHTML += "<p><strong>Low to High sorting:</strong> [" + v + "]<p>";
 
