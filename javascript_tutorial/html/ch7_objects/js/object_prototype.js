a9d38113c0b10ebd890abaa19c979cd15d44bfc5
Raul Zuniga <rzuniga64@gmail.com>
2016-06-29 02:07:29 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch7_objects/js/object_prototype.js
+++ javascript_tutorial/html/ch7_objects/js/object_prototype.js
@@ -1,5 +1,13 @@
+/**
+ * Prototypes can be applies to any object type in Javascript including Strings and Numbers.
+ * There is no method in JavaScript to reverse a string but we can make one ourselves and it will look like a native
+ * part of the language. Dangerous because if you start changing the default methods it could make your code
+ * incompatible with other developers.
+ */
+
 var msg = document.getElementById("message");
 
+// Take a String, splits it into an array, reverse it, and make it back into a string again.
 String.prototype.reverse = function() {
     return this.split("").reverse().join("");
 };
@@ -12,8 +20,11 @@ String.prototype.display = function () {
 
 s.display();
 
+// Don't extend native objects with your own methods!
+// If another developer came across this code it would fail when it came across a reverse function.
+// That is because it wouldn't know what to do with it. 'hasOwnProperty' returns true if a property
+// method directly belongs to String s.  It does not belong to an object further up the prototypical chain.
 for (var c in s) {
-    if (s.hasOwnProperty(c)) {
-        console.log(c);
-    }
+    //if (s.hasOwnProperty(c))
+        msg.innerHTML += "<p>" + c + "</p>";
 }
--- javascript_tutorial/html/ch7_objects/js/prototypical_inheritance.js
+++ javascript_tutorial/html/ch7_objects/js/prototypical_inheritance.js
@@ -1,4 +1,27 @@
-// JavaScript Object Prototypes
+/**
+ * Prototypical Inheritance
+ *
+ * Everything in Javascript is an object. The Car object is simply an instance of the base Object type.
+ * Every object you create is given a special prototype property which associates an object with its constructor.
+ * The display prototype does not need to be defined first. We can add it at any point even if we've created
+ * objects already.
+ *
+ * The prototype keyword says that display is a method for all objects of type Car.
+ * The display method is now associated with all Car objects. JavaScripts looks at the individual car objects and looks
+ * to see if it has a display method. c1 does not have an individual display method defined with the object itself.
+ * JavaScripts knows c1 is a type of object named Car so it looks for a display method defined there.
+ * It finds the display method below and runs it.
+ *
+ * There is something else unique to JavaScript. A prototype does not need to be defined first.  It can be added at any
+ * point even if objects have already been created.  The display method, for example, now applies to every instance
+ * of the Car object.  It applies to every object that has been created or will be created.
+ * 
+ * Javascript looks for a display method associated with the each object. 
+ * If none is found JavaScripts looks to see if there is a display method associated with the Car object. 
+ * If none is found it looks for one in the base Object.
+ *
+ * */
+
 var msg = document.getElementById("message");
 
 // car constructor
@@ -8,13 +31,7 @@ function Car(make, model, color) {
 	this.color = color || "unpainted";
 }
 
-/**
- * Every object you create is given a special prototype property which associates an object with its constructor 
- * function Our display prototype does not need to be defined first. We can add it at any point even if we've created 
- * ch7_objects already. Everything in jsvascript is an object. The car object is simply an instance of the Base object 
- * type. Eisplay method for all car ch7_objects
- */
-
+// Object.prototype.display = function() {
 Car.prototype.display = function() {
     msg.innerHTML += "<p>Your car is a "+this.color+" "+this.make+" "+this.model+".</p>";
 };
@@ -26,7 +43,10 @@ var c3 = new Car();
 
 // change properties and run methods
 c2.color = "red";
+// create a display method just for c1 Car
 c1.display = function() { msg.innerHTML += "<p>Not telling!</p>"; };
+
 c1.display();
+// The is not display function associated with c2 and c3 so it uses the car prototype and runs its display method.
 c2.display();
 c3.display();
