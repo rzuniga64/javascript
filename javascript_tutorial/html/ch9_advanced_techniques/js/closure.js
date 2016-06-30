83d80c6c035feb6715ed35c88d98ff745117c492
Raul Zuniga <rzuniga64@gmail.com>
2016-06-30 01:37:39 -0500

    Update javascriipt directory

---
+++ javascript_tutorial/html/ch9_advanced_techniques/js/closure.js
@@ -0,0 +1,15 @@
+/**
+ // Closure
+ // the value of the message is local to the function 'function'
+ // Once the code block ends the value of 'message' is lost
+ // JavaScript retains a reference to the 'message' variable because of the inner anonymous function.
+ */
+
+function CreateFunction() {
+	var message = "I was created by CreateFunction()"; // variable is local to the function
+	return function() {     // anonymous function that displays a message and is returned
+		alert(message);
+	};
+}
+var f = CreateFunction();   // assign function to a variable.
+f();                        // run the function
--- javascript_tutorial/html/ch9_advanced_techniques/js/library1.js
+++ javascript_tutorial/html/ch9_advanced_techniques/js/library1.js
@@ -3,7 +3,8 @@
  * The Format function, which formats a number,  is private. It cannot be accessed outside the anonymous function.
  * Once the anonymous function ends it is lost forever
  *
- * But any code inside the anonymous function can reference the function because of Closures.
+ * But any code inside the anonymous function can reference the Format function because of Closures.
+ * Call the function by using () and then put the whole anonymous function into braces
  */
 
 (function() {
@@ -13,4 +14,4 @@
 
     // call the ShowMessage function in library2.js
 	ShowMessage("You are viewing a lesson in section " + Format1(9.1));
-}() ); //() calls the function and then put the whole thing into braces
+}() );
