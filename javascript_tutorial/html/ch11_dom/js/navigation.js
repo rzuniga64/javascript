58985066970a5e81db8c60628a5181fcf6d1fc1f
Raul Zuniga <rzuniga64@gmail.com>
2016-07-01 16:49:19 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch11_dom/js/navigation.js
+++ javascript_tutorial/html/ch11_dom/js/navigation.js
@@ -5,10 +5,7 @@
  * Browsers internal representation of an HTML document is a tree. The 'html' tag is the root element. It has child
  * nodes 'head' and 'body'. Any node can have any number of child nodes but it can only have one parents, except for
  * the 'html' node.
- *
- * Navigation
  */
-
 console.clear();
 var n = document.getElementsByTagName("ul")[0];
 console.log( "ul node", n);
@@ -22,37 +19,34 @@ console.log( "childNodes", n.childNodes );
 console.log( "childNodes.length", n.childNodes.length );
 
 console.log( "li childNodes", n.getElementsByTagName("li") );
-
 console.log( "real childNodes", ElementNodes(n.childNodes) );
 
-// console.log( "previousSibling", n.previousSibling );
+// return real DOM nodelist
+function ElementNodes(nodelist) {
+    var eNodes = [];
+
+    for (var i=0, j=nodelist.length; i < j; i++) {
+        if (nodelist[i].nodeType == 1)  // look for real element( nodeType ==1)
+            eNodes.push(nodelist[i]);
+    }
+
+    return eNodes;
+}
+
+ //console.log( "previousSibling", n.previousSibling );
 // console.log( "nextSibling", n.nextSibling );
+
+// return real sibling (or null)
+function Sibling(node, type) {
+
+    do {
+        node = node[type];
+    } while (node && node.nodeType != 1);
+
+    return node;
+}
 
 console.log( "real previousSibling", Sibling(n, "previousSibling") );
 console.log( "real nextSibling", Sibling(n, "nextSibling") );
 
 
-// return real DOM nodelist
-function ElementNodes(nodelist) {
-	var eNodes = [];
-
-	for (var i=0, j=nodelist.length; i < j; i++) {
-		if (nodelist[i].nodeType == 1) {
-			eNodes.push(nodelist[i]);
-		}
-	}
-
-	return eNodes;
-}
-
-
-// return real sibling (or null)
-function Sibling(node, type) {
-
-	do {
-		node = node[type];
-	} while (node && node.nodeType != 1);
-
-	return node;
-
-}
