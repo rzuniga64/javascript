30668b8c6ef4d9e95679b12f0554ad0953c70211
Raul Zuniga <rzuniga64@gmail.com>
2016-07-02 16:44:26 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch13_form_handling/js/validate.js
+++ javascript_tutorial/html/ch13_form_handling/js/validate.js
@@ -1,16 +1,30 @@
-// form validation
-
-// DOM nodes
-var form = {
+/**
+ *  Form Events
+ *
+ *  Client-side JavaScript form validation is not a substitue for server-side data validation.  That's because
+ *  JavaScript can be disabled in the browser.
+ *
+ *  Use HTML5 form controls which provide JavaScript-less form validation. Input types:
+ *  - email
+ *  - date
+ *  - password
+ *
+ *  JavaScript is still required for custom validation.
+ *
+ *  Form event types:
+ *  - form submission
+ *  - field changes
+ *  - key presses
+ *
+ */
+var form = {    // DOM nodes
 	register:	document.getElementById("register"),
 	email:		document.getElementById("email"),
 	pass1:		document.getElementById("pass1"),
 	pass2:		document.getElementById("pass2")
 };
 
-// form submit
-// For browsers that understand HTML5 it won't be called until
-// the form is actually valid
+// Form submit: for browsers that understand HTML5 it won't be called until the form is actually valid
 form.register.addEventListener( "submit", CheckForm );
 
 // check email field when its changed, inline anonymous function
@@ -18,45 +32,30 @@ form.email.addEventListener( "change", function(e) {
 	if (e.target.value == "") alert("You forgot the email!");
 } );
 
-// stop space character
-form.pass1.addEventListener( "keypress", NoSpaces );
+form.pass1.addEventListener( "keypress", NoSpaces );        // stop space character
 form.pass2.addEventListener( "keypress", NoSpaces );
 
-// stop spaces being entered
-function NoSpaces(e) {
+function NoSpaces(e) {  // stop spaces being entered
 	if (e.charCode == 32) e.preventDefault();
 }
 
-// form submit validation
-var reEmail = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+var reEmail = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;    // form submit validation
 
-// Always run when a submit occurs
-// Won't be run on Chrome, Safari, Opera or Firefox until
-// the data is actually valid.
-function CheckForm(e) {
+/**
+ *  Always run when a submit occurs. Won't be run on Chrome, Safari, Opera or Firefox until the data is actually valid.
+ *  @param: event
+ */
+ function CheckForm(event) {
 
 	var msg = "";
 
 	// check email
-	if (!reEmail.test(form.email.value)) {
-		msg += "\nyour email address";
-	}
-
+	if (!reEmail.test(form.email.value)) msg += "\nyour email address";
 	// check passwords aren't empty and match each other
-	if (form.pass1.value == "" || form.pass1.value != form.pass2.value) {
-		msg += "\nyour passwords";
-	}
+	if (form.pass1.value == "" || form.pass1.value != form.pass2.value) msg += "\nyour passwords";
 
 	// complete message
-	if (msg != "") {
-		msg = "Please check:"+msg;
-	}
-	else {
-		msg = "Form is valid!\nSubmitting...";
-	}
-
+    msg = (msg != "" ? "Please check: " + msg : "Form is valid!\nSubmitting...");
 	alert(msg);
-
-	e.preventDefault();
-
+	event.preventDefault();
 }
