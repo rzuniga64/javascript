a44259730c2754023dd8d6f84e9c78d2c5f94723
Raul Zuniga <rzuniga64@gmail.com>
2016-07-02 16:14:58 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch12_event_handling/js/propagation.js
+++ javascript_tutorial/html/ch12_event_handling/js/propagation.js
@@ -1,24 +1,31 @@
-// event propagation
+/**
+ *  Event Propogation
+ *
+ *  The Event object tells which element received the event. By registering a single event handler you can monitor
+ *  every cell in a table by registering the parent element of all the cells.  This is called bubbling.
+ *
+ *  Step 1: Event Registration  Step 2: Write an event handler      Step 3: Further actions/
+ *  1. Identify the element     1. write a handler function         - disable default actions
+ *  2. Identify the action      2. passed an event object to it     - prevent 'bubbling'
+ *  3. Register a handler       3. analyze the event object         - deregister the event
+ *                              4. perform the relevant actions
+ */
 
 // find table
-var table = document.getElementById("multiplication");
-var tcols = table.getElementsByTagName("col");
+var table = document.getElementById("multiplication");  // table element
+var tcols = table.getElementsByTagName("col");          // collection of column elements in the table
 
 // delegate event handler
 table.addEventListener( "mouseover", TableHandler );
 table.addEventListener( "mouseout", TableHandler );
 
 // event handler
-function TableHandler(e) {
+function TableHandler(event) {
 
-	var t = e.target;
-	if (t.nodeName != "TD") return;
+	var target = event.target;                      // identify target node, a table cell
+	if (target.nodeName != "TD") return;            // we only care if the user hovers over a td cell
 	
-	// var cName = ( e.type == "mouseover" ? "active" : "");
-	// t.parentNode.className = cName;
-	// tcols[ t.cellIndex ].className = cName;
-
-	t.parentNode.className = tcols[ t.cellIndex ].className =
-	( e.type == "mouseover" ? "active" : "");
-	
+    var cName = ( event.type == "mouseover" ? "active" : "");
+	target.parentNode.className = cName;            // style the current node
+	tcols[ target.cellIndex ].className = cName;    // style the column
 }
