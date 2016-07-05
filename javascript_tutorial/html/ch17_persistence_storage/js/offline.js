edb1b248c71a3cfade956d66e816217659afa0bb
Raul Zuniga <rzuniga64@gmail.com>
2016-07-05 17:04:36 -0500

    Update javascriipt directory

--- javascript_tutorial/html/ch17_persistence_storage/js/offline.js
+++ javascript_tutorial/html/ch17_persistence_storage/js/offline.js
@@ -1,5 +1,17 @@
+/**
+ *  Local storage used when application goes offline
+ *
+ *  How do we make a web application work offline?
+ *  First we need a manifest file. This defines which file the browser should cache when connectivity is lost.
+ *  Manifest files need to be served using the text/cache-manifest MIME type. The server may do this for you but if
+ *  using a server like Apache you can create a .htacess file to ensure that it is done correctly. You then need to
+ *  change the HTML tag in your HTML file to point to the manifest file.  Now we need to determine when our application
+ *  goes offline or online and react accordingly.
+ *
+ */
+
 // offline HTML5 applications
-var online;
+var online;  // true when online, false when offline
 window.addEventListener("online", CheckOnlineStatus);
 window.addEventListener("offline", CheckOnlineStatus);
 CheckOnlineStatus();
@@ -19,7 +31,6 @@ function CheckOnlineStatus() {
 		s.textContent = "OFFLINE";
 		s.className = "offline";
 	}
-
 }
 
 
@@ -32,5 +43,4 @@ function Save() {
 	else {
 		// ... window.localStorage
 	}
-
 }
