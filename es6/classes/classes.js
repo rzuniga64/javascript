becef8dbedbc5b3035f7f9d0c1464c7556db839f
Raul Zuniga <rzuniga64@gmail.com>
2018-07-15 21:35:39 -0500

    update classes.js file.

--- es6/classes/classes.js
+++ es6/classes/classes.js
@@ -1,17 +1,17 @@
 // ES5
 function Person1() { }
 
-var person1 = new Person1();
+let person1 = new Person1();
 
 // ES6
-class Person2{
+class Person2 {
 
     constructor(name) {
         this.name = name;
     }
 
     greet() {
-        console.log(this.name);
+        console.log('Hello', this.name);
     }
 }
 
@@ -19,5 +19,5 @@ let person2 = new Person2('Max');
 person2.greet();
 
 // What is prototype of this person2 that we created here?
-console.log(person2.__proto__);
-console.log(person2.__proto__ === Person2.prototype);
+console.log('person2.__proto__ =' , person2.__proto__);
+console.log('(person2.__proto__ === Person2.prototype is', person2.__proto__ === Person2.prototype);
